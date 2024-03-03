02038a66bf893ae103db29896ebf8c2f8bb507bb
l@dforce.network <liujun.leo.lll@gmail.com>
2024-03-04 00:01:29 +0800

    update hardhat.config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,73 +1,87 @@
-import 'solidity-coverage'
-import 'hardhat-gas-reporter'
-import '@nomiclabs/hardhat-etherscan'
-import '@nomiclabs/hardhat-ethers'
-import '@nomiclabs/hardhat-waffle'
-import '@nomiclabs/hardhat-web3'
-import '@typechain/hardhat'
+import "solidity-coverage";
+import "hardhat-gas-reporter";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-web3";
+import "@typechain/hardhat";
+import "hardhat-deploy";
 
-import { HardhatUserConfig } from 'hardhat/config'
+import { HardhatUserConfig } from "hardhat/config";
 
-const testDir = process.env.TESTS_DIR ?? 'test'
+const testDir = process.env.TESTS_DIR ?? "test";
 
 const config: HardhatUserConfig = {
-  networks: {
-    ethereum: {
-      url: 'http://localhost:24012/rpc', // truffle-dashboard
-      timeout: 200000,
-    },
-    arbitrum: {
-      url: 'http://localhost:24012/rpc', // truffle-dashboard
-      timeout: 200000,
-    },
-    sepolia: {
-      url: 'http://localhost:24012/rpc', // truffle-dashboard
-      timeout: 2000000,
-    },
-    arbitrumSepolia: {
-      url: 'http://localhost:24012/rpc', // truffle-dashboard
-      timeout: 2000000,
-    },
-    localhost: {
-      url: 'http://localhost:24012/rpc', // truffle-dashboard
-      timeout: 200000,
-    },
-  },
-  mocha: {
-    timeout: 50000,
-  },
-  solidity: {
-    // note: we run optimizer only for dai.sol
-    compilers: [
-      {
-        version: '0.6.11',
-        settings: {
-          optimizer: {
-            enabled: false,
-          },
+    networks: {
+        ethereum: {
+            url: "http://localhost:24012/rpc", // truffle-dashboard
+            timeout: 200000,
         },
-      },
-    ],
-    overrides: {
-      'contracts/l2/dai.sol': {
-        version: '0.6.11',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
+        arbitrum: {
+            url: "http://localhost:24012/rpc", // truffle-dashboard
+            timeout: 200000,
         },
-      },
+        sepolia: {
+            url: "http://localhost:24012/rpc", // truffle-dashboard
+            // url: "https://eth-sepolia.g.alchemy.com/v2/mIp-juUsuqr9fAraZ2QHVBTp-xaYBbvM",
+            timeout: 2000000,
+            companionNetworks: {
+                l2: "arbitrumSepolia",
+            },
+        },
+        arbitrumSepolia: {
+            url: "http://localhost:24012/rpc", // truffle-dashboard
+            // url: "https://arbitrum-sepolia.blockpi.network/v1/rpc/public",
+            timeout: 2000000,
+            companionNetworks: {
+                l1: "sepolia",
+            },
+        },
+        localhost: {
+            url: "http://localhost:24012/rpc", // truffle-dashboard
+            timeout: 200000,
+        },
     },
-  },
-  paths: {
-    tests: testDir,
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS === '1',
-    currency: 'USD',
-    gasPrice: 50,
-  },
-}
+    mocha: {
+        timeout: 50000,
+    },
+    solidity: {
+        // note: we run optimizer only for dai.sol
+        compilers: [
+            {
+                version: "0.6.11",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+        ],
+        overrides: {
+            "contracts/l2/dai.sol": {
+                version: "0.6.11",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+        },
+    },
+    paths: {
+        deploy: "./scripts/deploy",
+        deployments: "./deployments",
+    },
+    namedAccounts: {
+        deployer: 0,
+    },
+    gasReporter: {
+        enabled: process.env.REPORT_GAS === "1",
+        currency: "USD",
+        gasPrice: 50,
+    },
+};
 
-export default config
+export default config;
